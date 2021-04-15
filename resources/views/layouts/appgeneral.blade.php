da948fb4aad76fa911de7442f9f95a43a9548156
sandeepa97 <lbsandeepa@gmail.com>
2021-04-15 21:25:10 +0530

    about us done

--- resources/views/layouts/appgeneral.blade.php
+++ resources/views/layouts/appgeneral.blade.php
@@ -13,6 +13,7 @@
 
 	<link rel="stylesheet" href="{{ url('css/general.css') }}">
 	<link rel="stylesheet" href="{{ url('css/store.css') }}">
+	<link href="{{url('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
 
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
